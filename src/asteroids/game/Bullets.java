df5a6752ceab808f3085409afdd5ab3c394a00eb
Cameron Steensma <>
2019-11-19 10:44:55 -0700

    Created Bullets and Debris classes extending Participant

---
+++ src/asteroids/game/Bullets.java
@@ -0,0 +1,24 @@
+package asteroids.game;
+
+import java.awt.Shape;
+
+public class Bullets extends Participant
+{
+    
+    
+
+    @Override
+    protected Shape getOutline ()
+    {
+        return null;
+        
+    }
+
+    @Override
+    public void collidedWith (Participant p)
+    {
+        
+        
+    }
+
+}
